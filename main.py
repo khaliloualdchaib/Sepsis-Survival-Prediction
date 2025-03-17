30f446e78d3ecf66fa4a9e067441ec658bf842cc
khaliloualdchaib <oualdchaibkhalil@gmail.com>
2025-03-17 07:13:48 +0100

    random forest

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn import tree
 from functions import *
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
 
 df1 = pd.read_csv("data/s41598-020-73558-3_sepsis_survival_primary_cohort.csv")
 df2 = pd.read_csv("data/s41598-020-73558-3_sepsis_survival_study_cohort.csv")
 df3 = pd.read_csv("data/s41598-020-73558-3_sepsis_survival_validation_cohort.csv")
 #combined all the data together
 df_combined = pd.concat([df1, df2, df3], ignore_index=True)
+df_combined.drop(columns=["episode_number"])
 
 #checked the distribution of all the classes
 # plt.rcParams.update({'font.size': 16})
@@ -27,20 +29,17 @@ X_resampled, y_resampled = smote.fit_resample(X, y)
 #split dataset
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
 
+
 param_grid = {
-    'max_depth': [3, 4, 5],  
-    'min_samples_split': [20, 30, 50],  
-    'min_samples_leaf': [20, 30, 40],  
-    'ccp_alpha': [0.001, 0.005, 0.01]  
+    'n_estimators': [100, 200, 300],
+    'max_depth': [10, 20, 30, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'class_weight': [None, 'balanced', 'balanced_subsample']
 }
 
-
-
-grid_search = GridSearchCV(tree.DecisionTreeClassifier(random_state=42, class_weight={0: 1, 1: 2}), param_grid, cv=5, scoring='precision')
+grid_search = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, scoring='precision', cv=5)
 grid_search.fit(X_train, y_train)
 
-best_dt = grid_search.best_estimator_
-print("Best Parameters:", grid_search.best_params_)
-show_tree(best_dt)
-
-evaluateModel(best_dt, X_test, y_test)
+best_rf = grid_search.best_estimator_
+evaluateModel(best_rf, X_test, y_test)
